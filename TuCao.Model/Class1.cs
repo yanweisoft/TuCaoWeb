dcaa54a60d869dc83953796f2ce256bc773e7942
yanweisoft <413665636@qq.com>
2013-12-17 10:01:43 +0800

    Class1.cs add Mehtod: GetData(int x,int y)

--- TuCao.Model/Class1.cs
+++ TuCao.Model/Class1.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace TuCao.Model
 {
     public class Class1
     {
+        public void LoadData()
+        {
+            string strName = "张三";
+        }
+
+        public SqlConnection GetSqlConnection()
+        {
+            string strConnction = "server=.;database=;uid=;pwd=;";
+            SqlConnection scn = new SqlConnection(strConnction);
+            return scn;
+        }
+
+        public int GetData(int x, int y)
+        {
+            int result = x + y;
+            return result;
+        }
     }
 }
