fd87b98adf00e32da1728231aa07523d957f8db9
yanweisoft <413665636@qq.com>
2013-12-19 10:57:05 +0800

    replay topic user  model

--- TuCao.Model/ReplyInfoModel.cs
+++ TuCao.Model/ReplyInfoModel.cs
@@ -5,7 +5,82 @@ using System.Text;
 
 namespace TuCao.Model
 {
+    /// <summary>
+    /// 回复内容
+    /// </summary>
     public class ReplyInfoModel
     {
+        private int _id;
+        private int _topicid;
+        private int _userid;
+        private string _nickname;
+        private string _postcontent;
+        private DateTime _createtime;
+        private string _ip;
+        private int _status;
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        /// <summary>
+        /// 帖子ID
+        /// </summary>
+        public int TopicId
+        {
+            get { return _topicid; }
+            set { _topicid = value; }
+        }
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public int UserId
+        {
+            get { return _userid; }
+            set { _userid = value; }
+        }
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        public string NickName
+        {
+            get { return _nickname; }
+            set { _nickname = value; }
+        }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string PostContent
+        {
+            get { return _postcontent; }
+            set { _postcontent = value; }
+        }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime
+        {
+            get { return _createtime; }
+            set { _createtime = value; }
+        }
+        /// <summary>
+        /// IP
+        /// </summary>
+        public string IP
+        {
+            get { return _ip; }
+            set { _ip = value; }
+        }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
     }
 }
--- TuCao.Model/TopicInfoModel.cs
+++ TuCao.Model/TopicInfoModel.cs
@@ -5,7 +5,110 @@ using System.Text;
 
 namespace TuCao.Model
 {
+
+    /// <summary>
+    /// 帖子内容
+    /// </summary>
     public class TopicInfoModel
     {
+        private int _id;
+        private int _userid;
+        private string _nickname;
+        private int _sex;
+        private int _typeid;
+        private string _postcontent;
+        private string _posttags;
+        private string _isemail;
+        private string _ip;
+        private DateTime _createtime;
+        private int _status;
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public int Userid
+        {
+            get { return _userid; }
+            set { _userid = value; }
+        }
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        public string Nickname
+        {
+            get { return _nickname; }
+            set { _nickname = value; }
+        }
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public int Sex
+        {
+            get { return _sex; }
+            set { _sex = value; }
+        }
+        /// <summary>
+        /// 类别ID
+        /// </summary>
+        public int Typeid
+        {
+            get { return _typeid; }
+            set { _typeid = value; }
+        }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Postcontent
+        {
+            get { return _postcontent; }
+            set { _postcontent = value; }
+        }
+        /// <summary>
+        /// 标签
+        /// </summary>
+        public string Posttags
+        {
+            get { return _posttags; }
+            set { _posttags = value; }
+        }
+        /// <summary>
+        /// 是否邮件通知
+        /// </summary>
+        public string Isemail
+        {
+            get { return _isemail; }
+            set { _isemail = value; }
+        }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime Createtime
+        {
+            get { return _createtime; }
+            set { _createtime = value; }
+        }
+        /// <summary>
+        /// IP
+        /// </summary>
+        public string Ip
+        {
+            get { return _ip; }
+            set { _ip = value; }
+        }
+        /// <summary>
+        /// 审核状态
+        /// </summary>
+        public int Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
     }
 }
