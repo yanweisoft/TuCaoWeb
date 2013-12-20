3603082cf8eca38237aa4779954bb03e5fe74206
yanweisoft <413665636@qq.com>
2013-12-20 10:20:02 +0800

    ReplyinfoDal.cs

--- TuCao.DAL/ReplyInfoDAL.cs
+++ TuCao.DAL/ReplyInfoDAL.cs
@@ -7,5 +7,13 @@ namespace TuCao.DAL
 {
     public class ReplyInfoDAL
     {
+        private const string sql_insert = @" INSERT INTO [ReplyInfo]( [TopicId],[UserId],[NickName] ,[PostContent],[CreateTime] ,[Ip] ,[Status]) VALUES( 
+                                               @TopicId, @UserId,@NickName,@PostContent,@CreateTime,@Ip,@Status)";
+        private const string sql_update = @" UPDATE [ReplyInfo] SET  ,[TopicId] = @TopicId,[UserId] = @UserId ,[NickName] = @NickName ,[PostContent] = @PostContent,[CreateTime] = @CreateTime  ,[Ip] = @Ip ,[Status] = @Status WHERE [Id] = @Id ";
+
+        private const string sql_delete = @" DELETE FROM [ReplyInfo]   WHERE Id=@Id ";
+
+        private const string sql_select = @"SELECT [Id],[TopicId] ,[UserId] ,[NickName] ,[PostContent],[CreateTime],[Ip],[Status] FROM [ReplyInfo] WHERE Id=@Id ";
+
     }
 }
