4eabcdc2da0d5283ba737d52cf3f67c819fb2ec7
yanweisoft <413665636@qq.com>
2013-12-25 12:01:14 +0800

    2

---
+++ TuCao.Model/RequestXML.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TuCao.Model
+{
+
+
+    #region 微信请求类
+    public class RequestXML
+    {
+
+        private string toUserName = "";
+        private string fromUserName = "";
+        private string createTime = "";
+        private string msgType = "";
+        private string content = "";
+        private string location_X = "";
+        private string location_Y = "";
+        private string scale = "";
+        private string label = "";
+        private string picUrl = "";
+        private string msgId = "";
+
+        /// <summary>
+        /// 开发者微信号
+        /// </summary>
+        public string ToUserName
+        {
+            get { return toUserName; }
+            set { toUserName = value; }
+        }
+
+        /// <summary>
+        /// 发送方帐号（一个OpenID） 
+        /// </summary>
+        public string FromUserName
+        {
+            get { return fromUserName; }
+            set { fromUserName = value; }
+        }
+
+        /// <summary>
+        /// 消息创建时间 （整型） 
+        /// </summary>
+        public string CreateTime
+        {
+            get { return createTime; }
+            set { createTime = value; }
+        }
+
+        /// <summary>
+        /// 消息类型  text image location link event  music news 
+        /// </summary>
+        public string MsgType
+        {
+            get { return msgType; }
+            set { msgType = value; }
+        }
+
+        /// <summary>
+        /// 文本消息内容 
+        /// </summary>
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        /// <summary>
+        /// 地理位置维度 
+        /// </summary>
+        public string Location_X
+        {
+            get { return location_X; }
+            set { location_X = value; }
+        }
+
+        /// <summary>
+        /// 地理位置精度 
+        /// </summary>
+        public string Location_Y
+        {
+            get { return location_Y; }
+            set { location_Y = value; }
+        }
+
+        /// <summary>
+        /// 地图缩放大小 
+        /// </summary>
+        public string Scale
+        {
+            get { return scale; }
+            set { scale = value; }
+        }
+
+        /// <summary>
+        /// 地理位置信息 
+        /// </summary>
+        public string Label
+        {
+            get { return label; }
+            set { label = value; }
+        }
+
+        /// <summary>
+        /// 图片链接 
+        /// </summary>
+        public string PicUrl
+        {
+            get { return picUrl; }
+            set { picUrl = value; }
+        }
+
+        /// <summary>
+        /// 消息id，64位整型 
+        /// </summary>
+        public string MsgId
+        {
+            get { return msgId; }
+            set { msgId = value; }
+        }
+
+    }
+
+    #endregion
+}
--- WebSites/Handler/WeiXin.ashx.cs
+++ WebSites/Handler/WeiXin.ashx.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using TuCao.Utility;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
 namespace WebSites.Handler
 {
     /// <summary>
@@ -11,10 +15,129 @@ namespace WebSites.Handler
     public class WeiXin : IHttpHandler
     {
 
-        public void ProcessRequest(HttpContext context)
+        HttpContext context = null;
+        string postStr = "";
+        public const string mytoken = "mytoken";
+        public void ProcessRequest(HttpContext param_context)
         {
+            context = param_context;
             context.Response.ContentType = "text/plain";
-            // context.Response.Write("Hello World");
+            #region 验证
+            valid();
+            #endregion
+
+            if (context.Request.HttpMethod.ToLower() == "post")
+            {
+                Stream s = context.Request.InputStream;
+                byte[] b = new byte[s.Length];
+                s.Read(b, 0, (int)s.Length);
+                postStr = Encoding.UTF8.GetString(b);
+                if (!string.IsNullOrEmpty(postStr))
+                {
+                    responseMsg(postStr);
+                }
+            }
+
+            //context.Response.Write("Hello World");
+        }
+
+
+
+
+        /// <summary>
+        /// 验证是否成为开发者
+        /// </summary>
+        public void valid()
+        {
+            var echostr = RequestQueryString.GetQueryString("echoStr");
+            if (checkSignature() && !string.IsNullOrEmpty(echostr))
+            {
+                context.Response.Write(echostr);
+                context.Response.End();//推送...不然微信平台无法验证token
+            }
+
+        }
+
+        /// <summary>
+        /// 验证签名
+        /// </summary>
+        /// <returns></returns>
+        public bool checkSignature()
+        {
+            var signature = RequestQueryString.GetQueryString("signature");
+            var timestamp = RequestQueryString.GetQueryString("timestamp"); ;
+            var nonce = RequestQueryString.GetQueryString("nonce"); ;
+            var token = mytoken;
+            string[] ArrTmp = { token, timestamp, nonce };
+            Array.Sort(ArrTmp);     //字典排序
+            string tmpStr = string.Join("", ArrTmp);
+            tmpStr = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(tmpStr, "SHA1");
+            tmpStr = tmpStr.ToLower();
+            if (tmpStr == signature)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void responseMsg(string postStr)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(postStr);
+            WriteLog("responseMsg:--------------" + postStr);
+            XmlElement rootElement = doc.DocumentElement;
+
+            XmlNode MsgType = rootElement.SelectSingleNode("doc");
+
+
+
+
+        }
+
+
+        private int ConvertDateTimeInt(DateTime time)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            return (int)(time - startTime).TotalSeconds;
+        }
+
+        private void WriteLog(string strMemo)
+        {
+            string filename = "d:/logs/log.txt";
+            if (!System.IO.Directory.Exists("d:/logs/"))
+            {
+                System.IO.Directory.CreateDirectory("d:/logs/");
+            }
+
+            StreamWriter sr = null;
+            try
+            {
+                if (!System.IO.File.Exists(filename))
+                {
+                    sr = System.IO.File.CreateText(filename);
+                }
+                else
+                {
+                    sr = System.IO.File.AppendText(filename);
+                }
+
+                sr.WriteLine(strMemo);
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+            }
         }
 
         public bool IsReusable
